c7d6dbf3728e9d08457f8afee444a64b3caf84e5
2001Krishanu <krish2001ghosh@gmail.com>
2022-01-06 20:43:02 +0530

    LetsChat

---
+++ app/src/main/java/com/example/letsChat/model/NotificationData.kt
@@ -0,0 +1,6 @@
+package com.example.letsChat.model
+
+data class NotificationData(
+    var title:String,
+    var message:String
+)
---
+++ app/src/main/java/com/example/letsChat/model/User.kt
@@ -0,0 +1,3 @@
+package com.example.letsChat.model
+
+data class User(var userId:String = "", var userName:String = "", var profileImage:String = "")
